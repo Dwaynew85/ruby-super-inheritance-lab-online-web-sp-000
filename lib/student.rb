d329f8fbfc031cf1cfeca62e25b61393a6f27d07
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-25 06:41:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
 
   def hello
-    "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
@@ -9,3 +9,4 @@ class Student
   end
 
 end
+    
